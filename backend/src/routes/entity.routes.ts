a9ded7ee6f3fd70b31a95f48e878f3bdbe5f9a19
Nina Lambon <ninalambon.nz@gmail.com>
2026-03-25 18:57:12 +1300

    debug: log company-details for listed company inspection

--- backend/src/routes/entity.routes.ts
+++ backend/src/routes/entity.routes.ts
@@ -154,6 +154,7 @@ router.get(
         }
 
         const data = await response.json();
+        console.log('NZBN company-details:', JSON.stringify(data['company-details']));
         externalEntityData = data;
       } catch (error) {
         if (error instanceof ApiError) throw error;
